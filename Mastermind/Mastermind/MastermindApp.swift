b7982541ccf4b92480c5c380882dd6f50f884ba9
Ignasi Perez-Valls <ignasiperez@gmail.com>
2024-05-07 18:12:52 +0200

    e - New Xcode (v15.3) project - Interface: SwiftUI, Language: Swift, Include Tests: off

---
+++ Mastermind/Mastermind/MastermindApp.swift
@@ -0,0 +1,17 @@
+//
+//  MastermindApp.swift
+//  Mastermind
+//
+//  Created by Ignasi Perez-Valls on 7/5/24.
+//
+
+import SwiftUI
+
+@main
+struct MastermindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
