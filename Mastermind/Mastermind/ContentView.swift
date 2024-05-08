448ec3386acaa1424d258d5294cb4a5aac56dd19
Ignasi Perez-Valls <ignasiperez@gmail.com>
2024-05-08 11:40:58 +0200

    t - Bypass SwiftUI App Launch During Unit Testing

--- Mastermind/Mastermind/ContentView.swift
+++ Mastermind/Mastermind/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
+            let _ = print("Production launch")
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
