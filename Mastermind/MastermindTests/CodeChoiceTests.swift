260ea74f2d0254c52e2a477a6b20be27d83854c5
Ignasi Perez-Valls <ignasiperez@gmail.com>
2024-07-15 18:40:58 +0200

    • t Add home for CodeChoice tests

---
+++ Mastermind/MastermindTests/CodeChoiceTests.swift
@@ -0,0 +1,13 @@
+//
+//  CodeChoiceTests.swift
+//  MastermindTests
+//
+//  Created by Ignasi Perez-Valls on 15/7/24.
+//
+
+@testable import Mastermind
+import XCTest
+
+final class CodeChoiceTests: XCTestCase {
+
+} // CodeChoiceTests
