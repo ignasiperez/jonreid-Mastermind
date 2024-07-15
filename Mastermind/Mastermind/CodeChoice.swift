8b26433dbed06886dc718ce61e4ed6cc7fafb33b
Ignasi Perez-Valls <ignasiperez@gmail.com>
2024-07-15 18:26:16 +0200

    - r Move CodeChoice stuff to own file

---
+++ Mastermind/Mastermind/CodeChoice.swift
@@ -0,0 +1,19 @@
+//
+//  CodeChoice.swift
+//  Mastermind
+//
+//  Created by Ignasi Perez-Valls on 15/7/24.
+//
+
+import SwiftUI
+
+struct CodeChoice {
+    let color: Color
+    let codeValue: Int
+}
+
+let codeChoice1 = CodeChoice(color: .brown, codeValue: 1)
+//  let codeChoice2 = CodeChoice(color: .black, codeValue: 2)
+let codeChoices = [codeChoice1]
+
+
