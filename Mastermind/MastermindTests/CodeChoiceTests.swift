352b56e6c1b8384a7eff136a49e74100582347ae
Ignasi Perez-Valls <ignasiperez@gmail.com>
2024-07-16 22:19:58 +0200

    - f Not enough colors is an error

--- Mastermind/MastermindTests/CodeChoiceTests.swift
+++ Mastermind/MastermindTests/CodeChoiceTests.swift
@@ -12,16 +12,24 @@ final class CodeChoiceTests: XCTestCase {
     func test_generateOneCodeChoice() throws {
         let result = try CodeChoiceGenerator.generate(from: [.red, .blue],
                                                       take: 1)
-        XCTAssertEqual(result, [CodeChoice(color: .red, codeValue: 1)])
+        XCTAssertEqual(result, [
+            CodeChoice(color: .red, codeValue: 1)
+        ])
     }
     
     func test_generateTwoCodeChoices() throws {
         let result = try CodeChoiceGenerator.generate(from: [.red, .blue],
                                                       take: 2)
         XCTAssertEqual(result, [
-                        CodeChoice(color: .red, codeValue: 1),
-                        CodeChoice(color: .blue, codeValue: 2)
-                       ])
+            CodeChoice(color: .red, codeValue: 1),
+            CodeChoice(color: .blue, codeValue: 2)
+        ])
     }
-
+    
+    func test_notEnoughColorsIsAnError() throws {
+        XCTAssertThrowsError(
+            try CodeChoiceGenerator.generate(from: [.red, .blue], take: 3)
+        )
+    }
+    
 } // CodeChoiceTests
