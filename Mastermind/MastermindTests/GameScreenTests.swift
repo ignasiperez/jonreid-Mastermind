933e2c79435a7d88435d2f8f3e9f9c03709bdc0c
Ignasi Perez-Valls <ignasiperez@gmail.com>
2024-07-19 14:13:42 +0200

    • t Remove precondition check from screen test

--- Mastermind/MastermindTests/GameScreenTests.swift
+++ Mastermind/MastermindTests/GameScreenTests.swift
@@ -18,9 +18,8 @@ final class GameScreenTests: XCTestCase {
     @MainActor func test_tappingCodeChoiceSetsGuessColor() throws {
         let game = try Game(numberOfCodeChoices: 1)
         var sut = GameScreen(game: game)
-        var color = try getColorOfGuess(try sut.inspect())
         let codeChoice = game.codeChoices[0]
-        XCTAssertNotEqual(color, codeChoice.color, "Precondition")
+        var color: Color?
         
         update(&sut) { view in
             // Tap code choice button
